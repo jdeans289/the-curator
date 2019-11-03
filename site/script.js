22f1d2c34b1307d2ff9a0444df6cb74621331723
jdeans289 <jdeans289@gmail.com>
2019-11-03 00:29:07 -0500

    aaaaaahhhhhh why wont it work

--- site/script.js
+++ site/script.js
@@ -4,36 +4,61 @@
 //     -d "extractors=entities,entailments" \
 //     -d "text=Spain's stricken Bankia expects to sell off its vast portfolio of industrial holdings that includes a stake in the parent company of British Airways and Iberia." \
 //     https://api.textrazor.com/
-var text = "Mueller interview notes obtained by CNN show Trump's push for stolen emails";
-var text_enc = encodeURI(text);
-var url_keywords = 'https://api.textrazor.com?' +
-                    'extractors=entities,entailments&' + 
-                    'text=' + text_enc;
+// var text = "Mueller interview notes obtained by CNN show Trump's push for stolen emails";
+// var text_enc = encodeURI(text);
+// var url_keywords = 'https://api.textrazor.com?' +
+//                     'extractors=entities,entailments&' +
+//                     'text=' + text_enc;
+//
+// var url2 = "http://api.textrazor.com/";
+// var req_keywords = new Request(url_keywords);
+// const data = { text : text,
+//                extractors: "entities"};
+//
+// fetch(url2, {
+//         method: 'POST',
+//         mode: 'no-cors',
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded',
+//             'x-textrazor-key': 'e1ee3ad8e80c6865b69dde454b7febf529c37874a94542d05dae384b'
+//         },
+//         body: {text : text,
+//             extractors: "entities"
+//         }
+//     })
+//     .then(function(response) {
+//           if(response.ok) { // Check if response went through
+//               response.json().then(function(data) {
+//                   console.log(data);
+//                   // var output = document.getElementById('articlelist');
+//                   // for (var i = 0; i < data.articles.length ; i++) {
+//                   //     var articleinfo = '<a href=' + data.articles[i].url + '>' +data.articles[i].title +'</a>';
+//                   //     articleinfo += '</p>Source: ' + data.articles[i].source.name + '</p>';
+//                   //     output.innerHTML += articleinfo;
+//                   // }
+//               });
+//           } else { // Response wasn't ok. Check dev tools
+//               console.log("response failed?");
+//               console.log(response)
+//           }
+//     });
 
+var settings = {
+  "url": "https://api.textrazor.com",
+  "method": "POST",
+  "headers": {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "x-textrazor-key": "e1ee3ad8e80c6865b69dde454b7febf529c37874a94542d05dae384b"
+  },
+  "body": {
+    "text": "Mueller interview notes obtained by CNN show Trump's push for stolen emails",
+    "extractors": "entities"
+  }
+}
 
-var req_keywords = new Request(url_keywords);
-
-fetch(url_keywords, { headers: {
-            'x-textrazor-key': 'e1ee3ad8e80c6865b69dde454b7febf529c37874a94542d05dae384b'
-        }}
-     )
-    .then(function(response) {
-          if(response.ok) { // Check if response went through
-              response.json().then(function(data) {
-                  console.log(data);
-                  // var output = document.getElementById('articlelist');
-                  // for (var i = 0; i < data.articles.length ; i++) {
-                  //     var articleinfo = '<a href=' + data.articles[i].url + '>' +data.articles[i].title +'</a>';
-                  //     articleinfo += '</p>Source: ' + data.articles[i].source.name + '</p>';
-                  //     output.innerHTML += articleinfo;
-                  // }
-              });
-          } else { // Response wasn't ok. Check dev tools
-              console.log("response failed?");
-          }
-    });
-
-
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
 
 // Get articles
 var str = "Mueller AND Trump AND stolen AND emails AND interview"
